88e2436abee149b1085898019ee22d63a3e8164b
René Løwe Jacobsen <rlj@rloewe.net>
2018-12-20 11:07:23 +0100

    Changed bot to react to discord heartbeat

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
 import discord
 import asyncio
 import configparser
@@ -5,6 +7,9 @@ import queue
 import threading
 import requests
 import time
+import pickle
+import json
+import os
 from prettytable import PrettyTable
 from lxml import html
 
@@ -19,22 +24,42 @@ class machine:
     def gettype(self):
         return self.name.split(" ")[0]
 
-    def tostring(self):
-        return u" ".join([self.name, self.price, self.status, self.timeleft, self.started])
-
     def isavailable(self):
         return self.status.lower() == "fri"
 
     def _convert_nbsp(self, string):
         return string.replace("\xa0", " ")
 
+    def __str__(self):
+        return u" ".join([
+            self.name,
+            self.price,
+            self.status,
+            self.timeleft,
+            self.started
+            ])
+
 class laundry:
-    def __init__(self, url):
-        self.url = url
-        self.availablemachines = self.getmachines()
+    def __init__(self, ip, url):
+        self.url = "http://" + ip + url
+
+        if os.path.isfile(".session"):
+            with open(".session", "rb") as f:
+                self._session = pickle.load(f)
+        else:
+            self._session = requests.Session()
+
+        # Make sure system is up and running
+        self._session.get("http://" + ip)
+
+        self.dumpsession()
+
+        self.availablemachines = []
+
 
     def getmachines(self):
-        req = requests.get(self.url)
+        req = self._session.get(self.url)
+        self.dumpsession()
         machines = []
 
         if req.status_code == 200:
@@ -52,11 +77,20 @@ class laundry:
                                 )
                             )
             return machines
+        elif req.status_code == 403:
+            #TODO: fix busy webserver
+            # Session cookies have been added as a possible fix for this
+            print("Server says it is busy")
         else:
+            print(req.status_code)
+            print(req.text)
             print("something went wrong")
             return []
 
     def machineexists(self, name):
+        if len(self.availablemachines) == 0:
+            fixlocalcache()
+
         for machine in self.availablemachines:
             if machine.name.lower() == name:
                 return True
@@ -71,8 +105,19 @@ class laundry:
 
     def getstatustable(self):
         machines = self.getmachines()
-        table = PrettyTable(["navn", "pris", "status", "tid tilbage", "startet"])
-        [table.add_row([x.name, x.price, x.status, x.timeleft, x.started]) for x in machines]
+        table = PrettyTable([
+            "navn",
+            "pris",
+            "status",
+            "tid tilbage",
+            "startet"])
+        [table.add_row([
+            x.name,
+            x.price,
+            x.status,
+            x.timeleft,
+            x.started
+            ]) for x in machines]
         return table.get_string()
 
     def availableoftype(self, machinetype):
@@ -83,40 +128,31 @@ class laundry:
                 available.append(machine)
         return machines
 
+    def fixlocalcache(self):
+        self.availablemachines = self.getmachines()
+
+    def dumpsession(self):
+        with open(".session", "wb") as f:
+            pickle.dump(self._session, f)
+
 class job:
-    def __init__(self, mention, cmd):
+    def __init__(self, channel, mention, cmd):
         self.mention = mention
+        self.channel = channel
         self.cmd = cmd
 
-class notifier(threading.Thread):
-    def run(self):
-        myjobs = []
-        while True:
-            time.sleep(60)
-            while not jobs.empty():
-                myjobs.append(jobs.get())
-            print(client)
-            machines = l.getmachines()
-            for job in myjobs:
-                if job.cmd.cmd == "mangler":
-                    for machine in machines:
-                        if machine.gettype() == job.cmd.machinetype:
-                            print("lul")
-                            break
-                elif job.cmd.cmd == "bruger":
-                    for machine in machines:
-                        if machine.name == job.cmd.machine:
-                            print("lul")
-                            break
-
+    def __str__(self):
+        return " ".join([self.mention, str(self.channel), str(self.cmd)])
 
 client = discord.Client()
 config = configparser.ConfigParser()
 config.read("vask.ini")
 token = config["DEFAULT"]["token"]
+ip = config["DEFAULT"]["ip"]
 url = config["DEFAULT"]["url"]
 jobs = queue.Queue()
-l = laundry(url)
+l = laundry(ip, url)
+myjobs = []
 
 @client.event
 async def on_ready():
@@ -124,6 +160,49 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("")
+
+@client.event
+async def on_socket_raw_receive(msg):
+    if isinstance(msg, str):
+        event = json.loads(msg)
+        # React to heartbeet event
+        if isinstance(event, dict) and event["op"] == 11:
+            while not jobs.empty():
+                myjobs.append(jobs.get())
+
+            if len(myjobs) > 0:
+                machines = l.getmachines()
+                donejobs = []
+                for job in myjobs:
+                    print(job)
+                    if job.cmd["cmd"] == "mangler":
+                        for machine in machines:
+                            print(machine.gettype())
+                            if machine.gettype().lower() == job.cmd["machinetype"].lower() and machine.isavailable():
+                                await client.send_message(
+                                        job.channel,
+                                        job.mention
+                                            + " en maskine af typen "
+                                            + job.cmd["machinetype"]
+                                            + " er nu ledig!"
+                                        )
+                                donejobs.append(job)
+                                break
+                    elif job.cmd["cmd"] == "bruger":
+                        for machine in machines:
+                            if machine.name.lower() == job.cmd["machine"].lower() and machine.isavailable():
+                                await client.send_message(
+                                        job.channel,
+                                        job.mention
+                                            + u" dit tøj i maskine "
+                                            + job.cmd["machine"]
+                                            + u" er nu færdigt!"
+                                        )
+                                donejobs.append(job)
+                                break
+                for job in donejobs:
+                    myjobs.remove(job)
+            print(msg)
 
 @client.event
 async def on_message(message):
@@ -134,29 +213,67 @@ async def on_message(message):
         args = parts[2:]
 
         if cmd == "status":
-            machines = l.getmachines()
-            await client.send_message(message.channel, message.author.mention + "```\n" + l.getstatustable()  +  "```")
+            await client.send_message(
+                    message.channel,
+                    message.author.mention
+                        + "```\n"
+                        + l.getstatustable()
+                        +  "```"
+                    )
         elif cmd == "bruger" and len(args) > 0:
             name = " ".join(args).lower()
             if not l.machineexists(name):
-                await client.send_message(message.channel, message.author.mention + " maskinen findes ikke")
+                await client.send_message(
+                        message.channel,
+                        message.author.mention + " maskinen findes ikke"
+                        )
                 return
 
             if not l.ismachineinuse(name):
-                await client.send_message(message.channel, message.author.mention + " maskinen er fri lige nu. Så er dit vasketøj færdigt?")
+                await client.send_message(
+                        message.channel,
+                        message.author.mention
+                            + " maskinen er fri lige nu. Så er dit vasketøj færdigt?"
+                        )
                 return
 
-            await client.send_message(message.channel, message.author.mention + " Jeg holder øje med dit vasketøj")
-            jobs.put(job(mention, { cmd: cmd, machine: name }))
+            await client.send_message(
+                    message.channel,
+                    message.author.mention + " Jeg holder øje med dit vasketøj"
+                    )
+            jobs.put(
+                    job(
+                        message.channel,
+                        message.author.mention,
+                        { "cmd": cmd, "machine": name }
+                        )
+                    )
         elif cmd == "mangler" and len(args) > 0:
             available = l.availableoftype(args[0])
             if len(available) > 0:
-                await client.send_message(message.channel, message.author.mention + "Der er en ledig maskine af den type lige nu!")
+                await client.send_message(
+                        message.channel,
+                        message.author.mention
+                            + "Der er en ledig maskine af den type lige nu!"
+                        )
+                return
 
-            await client.send_message(message.channel, message.author.mention + " Du får besked, når der er en fri maskine")
-            jobs.put(job(mention, { cmd: cmd, machinetype: args[0] }))
+            await client.send_message(
+                    message.channel,
+                    message.author.mention
+                        + " Du får besked, når der er en fri maskine"
+                    )
+            jobs.put(
+                    job(
+                        message.channel,
+                        message.author.mention,
+                        { "cmd": cmd, "machinetype": args[0] }
+                        )
+                    )
         elif cmd == "help":
-            await client.send_message(message.channel, """```
+            await client.send_message(
+            message.channel,
+            """```
 Jeg tager i mod følgende kommandoer:
 status - få status over alle maskiner
 bruger [maskine] - giver en notifikation, når maskinen er færdig
@@ -164,5 +281,4 @@ mangler [type] - giver en notifikation, når maskintype er ledig
 help - denne besked
             ```""")
 
-notifier().start()
 client.run(token)
